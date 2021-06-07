e2e1140252822a50bfa4078237587cb743b945b3
jonathan <jb@amazing-discoveries.org>
2021-06-07 16:00:05 +0200

    migrate from os to pathlib, fix service path for multi channel setup

--- ffplayout/apps/api_player/serializers.py
+++ ffplayout/apps/api_player/serializers.py
@@ -1,5 +1,5 @@
 import configparser
-import os
+from pathlib import Path
 from shutil import copyfile
 
 from apps.api_player.models import GuiSettings, MessengePresets
@@ -7,18 +7,20 @@ from apps.api_player.utils import read_yaml, write_yaml
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+CONF_PATH = Path('/etc/ffplayout/supervisor/conf.d/')
 
-def create_engine_config(_path, yml_config):
-    suffix = os.path.basename(_path).split('-')[1].split('.')[0]
+
+def create_engine_config(service, yml_config):
+    suffix = service.split('-')[1]
     config = configparser.ConfigParser()
-    config.read('/etc/ffplayout/supervisor/conf.d/engine-001.conf')
+    config.read(CONF_PATH.joinpath('engine-001.conf'))
     items = config.items('program:engine-001')
 
     config.add_section(f'program:engine-{suffix}')
 
     for (key, value) in items:
         if key == 'command':
-            value = ('/opt/ffplayout-engine/venv/bin/python ffplayout.py '
+            value = ('/opt/ffplayout_engine/venv/bin/python ffplayout.py '
                      f'-c {yml_config}')
         elif key == 'stdout_logfile':
             value = f'/var/log/ffplayout/engine-{suffix}.log'
@@ -26,7 +28,7 @@ def create_engine_config(_path, yml_config):
 
     config.remove_section('program:engine-001')
 
-    with open(_path, 'w') as file:
+    with open(CONF_PATH.joinpath(f'{service}.conf'), 'w') as file:
         config.write(file)
 
 
@@ -70,54 +72,52 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class GuiSettingsSerializer(serializers.ModelSerializer):
-
     def create(self, validated_data):
         settings = GuiSettings.objects.create(**validated_data)
+        config = Path(validated_data['playout_config'])
 
-        if not os.path.isfile(validated_data['engine_service']):
-            create_engine_config(validated_data['engine_service'],
-                                 validated_data['playout_config'])
-        if not os.path.isfile(validated_data['playout_config']):
-            suffix = os.path.basename(
-                validated_data['playout_config']).split('-')[1].split('.')[0]
+        if not CONF_PATH.joinpath(
+                f'{validated_data["engine_service"]}.conf').is_file():
+            create_engine_config(validated_data['engine_service'], str(config))
+        if not config.is_file():
+            suffix = config.stem.split('-')[1]
             yaml_obj = read_yaml(1)
-            old_log_path = yaml_obj['logging']['log_path'].rstrip('/')
-            old_pls_path = yaml_obj['playlist']['path'].rstrip('/')
+            old_log_path = Path(yaml_obj['logging']['log_path'])
+            old_pls_path = Path(yaml_obj['playlist']['path'])
 
-            if os.path.basename(old_log_path) == 'ffplayout':
-                log_path = f'{old_log_path}/channel-{suffix}'
+            if old_log_path.name == 'ffplayout':
+                log_path = old_log_path.joinpath(f'channel-{suffix}')
             else:
-                log_path = f'{os.path.dirname(old_log_path)}/channel-{suffix}'
+                log_path = old_log_path.parent.joinpath(f'channel-{suffix}')
 
-            if os.path.basename(old_pls_path) == 'playlists':
-                play_path = f'{old_pls_path}/channel-{suffix}'
+            if old_pls_path.name == 'playlists':
+                play_path = old_pls_path.joinpath(f'channel-{suffix}')
             else:
-                play_path = f'{os.path.dirname(old_pls_path)}/channel-{suffix}'
+                play_path = old_pls_path.parent.joinpath(f'channel-{suffix}')
 
-            yaml_obj['logging']['log_path'] = log_path
-            yaml_obj['playlist']['path'] = play_path
+            yaml_obj['logging']['log_path'] = str(log_path)
+            yaml_obj['playlist']['path'] = str(play_path)
             yaml_obj['out']['stream_output'] = \
                 yaml_obj['out']['stream_output'] + suffix
 
-            if not os.path.isdir(log_path):
-                os.makedirs(log_path, exist_ok=True)
+            if not log_path.is_dir():
+                log_path.mkdir(exist_ok=True)
 
-            if not os.path.isdir(play_path):
-                os.makedirs(play_path, exist_ok=True)
+            if not play_path.is_dir():
+                play_path.mkdir(exist_ok=True)
 
             write_yaml(yaml_obj, settings.id)
 
         return settings
 
     def update(self, instance, validated_data):
-        if not os.path.isfile(validated_data['engine_service']) and \
-            os.path.isfile(
-                '/etc/ffplayout/supervisor/conf.d/engine-001.conf'):
-            create_engine_config(validated_data['engine_service'],
-                                 validated_data['playout_config'])
-        if not os.path.isfile(validated_data['playout_config']):
-            copyfile('/etc/ffplayout/ffplayout-001.yml',
-                     validated_data['playout_config'])
+        service = validated_data['engine_service']
+        config = Path(validated_data['playout_config'])
+        if not CONF_PATH.joinpath(f'{service}.conf').is_file() and \
+                CONF_PATH.joinpath('engine-001.conf').is_file():
+            create_engine_config(service, str(config))
+        if not config.is_file():
+            copyfile('/etc/ffplayout/ffplayout-001.yml', str(config))
 
         instance.channel = validated_data.get('channel', instance.channel)
         instance.player_url = validated_data.get('player_url',
